6ce2a10352dd50d0aa24159d52a4dd3071895106
Gabriel Nogueira <51515680+Talendar@users.noreply.github.com>
2020-12-06 14:40:07 -0300

    Add files via upload

---
+++ aux/eval.py
@@ -0,0 +1,98 @@
+""" Auxiliary functions related to model evaluation.
+
+@author: Gabriel Nogueira (Talendar)
+"""
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+mape = tf.keras.metrics.MeanAbsolutePercentageError()
+mae = tf.keras.metrics.MeanAbsoluteError()
+mse = tf.keras.metrics.MeanSquaredError()
+
+
+def eval_plot(predictions, labels, start_date, plot_samples, title):
+    if len(labels) != len(predictions):
+        labels = labels[:len(predictions)]
+
+    # sampling
+    idx = np.random.randint(len(labels.index) - plot_samples) \
+          if start_date == "random" \
+          else labels.index.index(start_date)
+
+    sl = slice(idx, idx + plot_samples)
+    predictions, labels = predictions[sl], labels[sl]
+
+    # plotting
+    fig = plt.figure(figsize=(18, 9))
+    fig.subplots_adjust(top=2.6)
+    fig.suptitle(title, fontsize=22, color="#E0E0E0", x=0.52, y=2.76)
+
+    for i, col in enumerate(labels.columns):
+        col_title = "Opening Price" if col == "open" else \
+                    "Closing Price" if col == "close" else \
+                    "Lowest Price"  if col == "low" else \
+                    "Highest Price" if col == "high" else col
+
+        # error bars
+        bars_errors = [[], []]
+        for d in (labels[col] - predictions[col]).values:
+            if d > 0:
+                bars_errors[1].append(d)
+                bars_errors[0].append(0)
+            else:
+                bars_errors[0].append(-d)
+                bars_errors[1].append(0)
+
+        # config
+        ax_graph = fig.add_subplot(len(labels.columns), 1, i+1)
+        ax_graph.tick_params(axis='both', which='major', labelsize=12, color="#DCDCDC")
+        ax_graph.set_title(f"\n{col_title}\n", fontsize=18, color="#E0E0E0")
+        ax_graph.set_ylabel('Ibovespa\n', fontsize=16, color="#E0E0E0")
+
+        # plot
+        ax_graph.plot(labels[col].index, labels[col].values, 
+                marker='o', c='#2ca02c', 
+                label='Labels', linewidth=3)
+        ax_graph.errorbar(predictions[col].index, predictions[col].values, ecolor="#ff0000",
+                    yerr=bars_errors, linestyle="--", elinewidth=2,
+                    marker='s', label='Predictions', c='#ff7f0e', 
+                    alpha=0.6, linewidth=3)[-1][0].set_linestyle(':')
+        ax_graph.legend(fontsize=16)
+
+
+def eval_print(results):
+    print("#"*25)
+    for col in results:
+        print(
+            f"\n[> {col.upper()} <]\n" +
+            f"   . MAE:  {results[col]['mae'] : .0f}\n" +
+            f"   . MAPE: {results[col]['mape'] : .4f}%\n"
+            f"   . MSE:  {results[col]['mse'] : .0f}\n" + 
+            "\n" + "#" * 25
+        )
+
+
+def eval(model, data, set_name):
+    # predictions
+    predictions = model.predict(data.dataset(set_name))
+    labels = data.raw_data(set_name)["labels"][:len(predictions)]
+    predictions = pd.DataFrame(data=predictions, 
+                               index=labels.index,
+                               columns=labels.columns)
+    
+    if data.denormalize_labels is not None:
+        predictions = data.denormalize_labels(predictions)
+
+    # results
+    results = {"general": {"mae": mae(denorm_labels, predictions),
+                           "mape": mape(denorm_labels, predictions),
+                           "mse": mse(denorm_labels, predictions)} }
+
+    for col in denorm_labels.columns:
+        results[col] = {"mae":  mae(denorm_labels[col], predictions[col]),
+                        "mape": mape(denorm_labels[col], predictions[col]),
+                        "mse":  mse(denorm_labels[col], predictions[col])}
+
+    return predictions, results
---
+++ aux/stocks_data.py
@@ -0,0 +1,105 @@
+""" Implements a custom class to help managing the stocks data.
+
+@author: Gabriel Nogueira (Talendar)
+"""
+
+
+class StocksData:
+    def __init__(self,
+                 data_df,
+                 num_sessions,
+                 labels_names,
+                 feature_normalization=None,  # tuple: (norm_func, denorm_func)
+                 label_normalization=None,  # tuple: (norm_func, denorm_func)
+                 data_split_pc=(.8, .12, .08)):
+        self.raw = data_df
+        self._num_sessions = num_sessions
+        self._labels_names = labels_names
+
+        # cleaning
+        if "symbol" in data.index.names:
+            self.raw.reset_index("symbol", inplace=True, drop=True)
+
+        if "adjclose" in self.raw.columns:
+            self.raw.drop("adjclose", axis=1, inplace=True)
+
+        # splitting
+        self.raw_train = self.raw_val = self.raw_test = None
+        self._split_data(data_split_pc)
+
+        # normalization
+        self.norm_train = {"features": None, "labels": None}
+        self.norm_val = {"features": None, "labels": None}
+        self.norm_test = {"features": None, "labels": None}
+
+        if feature_normalization is not None:
+            norm_f, self.denormalize_features = feature_normalization
+            self._norm_features_params = self._normalization(norm_f, "features")
+
+        if label_normalization is not None:
+            norm_f, self.denormalize_labels = label_normalization
+            self._norm_labels_params = self._normalization(norm_f, "labels")
+
+        # datasets
+        self.train_ds = self._make_ds(self.raw_train, self.norm_train)
+        self.val_ds = self._make_ds(self.raw_val, self.norm_val)
+        self.test_ds = self._make_ds(self.raw_test, self.norm_test)
+
+    def raw_data(self, set_name):
+        return getattr(self, "raw_" + set_name)
+    
+    def norm_data(self, set_name):
+        return getattr(self, "norm_" + set_name)
+
+    def dataset(self, set_name):
+        return getattr(self, set_name + "_ds")
+
+    def _split_data(self, pc):
+        assert np.sum(pc) == 1, "Data split percentage sum must be 1 (100%)!"
+        n = len(self.raw)
+
+        # splitting
+        train_data, train_labels = self._features_and_labels(
+            self.raw[:int(n*pc[0])])
+        val_data, val_labels = self._features_and_labels(
+            self.raw[int(n*pc[0]) : int(n * (pc[0] + pc[1]))])
+        test_data, test_labels = self._features_and_labels(
+            self.raw[int(n * (pc[0] + pc[1])):])
+
+        # asserting labels
+        self._assert_labels(train_data, train_labels)
+        self._assert_labels(val_data, val_labels)
+        self._assert_labels(test_data, test_labels)
+
+        # dict
+        self.raw_train = {"features": train_data, "labels": train_labels}
+        self.raw_val = {"features": val_data, "labels": val_labels}
+        self.raw_test = {"features": test_data, "labels": test_labels}
+
+    def _features_and_labels(self, data):
+        return data[:-self._num_sessions], data[self._labels_names][self._num_sessions:]
+
+    def _assert_labels(self, data, labels):
+        data, labels = data[self._labels_names].values, labels.values
+        for i in range(len(labels) - self._num_sessions):
+            assert np.array_equal(labels[i], data[i + self._num_sessions])
+
+    def _normalization(self, norm, name):
+        params = norm(self.raw_train[name])
+        self.norm_train[name], params = params[0], params[1:]
+        self.norm_val[name] = norm(self.raw_val[name], *params)[0]
+        self.norm_test[name] = norm(self.raw_test[name], *params)[0]
+        return params
+
+    def _make_ds(self, data, norm_data):
+        return tf.keras.preprocessing.timeseries_dataset_from_array(
+            data=data["features"] if norm_data["features"] is None 
+                                  else norm_data["features"],
+            targets=data["labels"] if norm_data["labels"] is None
+                                   else norm_data["labels"],
+            sequence_length=self._num_sessions,
+            sequence_stride=1,
+            shuffle=False,
+            batch_size=32)
+        
+
