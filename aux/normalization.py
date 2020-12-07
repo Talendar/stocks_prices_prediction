86bbbd8190379ab3ff6932087573c6f7faaa76af
Gabriel Nogueira <51515680+Talendar@users.noreply.github.com>
2020-12-07 16:53:57 -0300

    Update normalization.py

--- aux/normalization.py
+++ aux/normalization.py
@@ -11,8 +11,24 @@ def min_max_norm(data, dmin=None, dmax=None):
     if dmax is None:
         dmax = data.max(axis=0)
 
-    return (data - dmin) / (dmax - dmin), dmin, dmax
+    return (data - dmin) / (dmax - dmin), \
+           dmin, dmax
 
 
 def min_max_denorm(data, dmin, dmax):
     return (data * (dmax - dmin)) + dmin
+
+
+def zscore_norm(data, mean=None, std=None):
+    if mean is None:
+        mean = data.mean(axis=0)
+
+    if std is None:
+        std = data.std(axis=0)
+
+    return (data - mean) / std, \
+           mean, std
+
+
+def zscore_denorm(data, mean, std):
+    return data * std + mean
